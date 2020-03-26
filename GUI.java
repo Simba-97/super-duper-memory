7388b6f5f631276a4fb3cbe0138ebf509b04e069
Hritik Singh <55483056+singhHritik97@users.noreply.github.com>
2020-03-26 22:29:36 +0530

    GUI commit on the 26th of March

---
+++ GUI.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+public class GUI implements ActionListener {
+
+    int count = 0;
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+    JButton button;
+
+
+    public GUI(){
+        frame = new JFrame();
+
+        button = new JButton("Click me");
+        button.addActionListener(this);
+        label = new JLabel("Number Of Clicks: 0");
+
+        panel = new JPanel();
+
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("OUR GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[]args){
+        new GUI();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        count++;
+        label.setText("Number of clicks:" + count);
+    }
+}
